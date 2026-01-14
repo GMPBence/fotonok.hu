1f9ef1831c8c814a1a99d321497548b719caad40
Botond-cyber <david.botond.bajan@students.jedlik.eu>
2026-01-14 08:25:43 +0100

    Update PaymentPage layout for improved responsiveness and text alignment

--- src/pages/PaymentPage.jsx
+++ src/pages/PaymentPage.jsx
@@ -9,10 +9,10 @@ const PaymentPage = (props) => {
       <Navbar authenticated={props.authenticated} />
       <div className="flex flex-col justify-center items-center gap-7 px-2">
         <div className="flex flex-col items-center">
-          <h1 className="text-4xl font-extrabold">Vásárlás összegzés</h1>
+          <h1 className="text-4xl font-extrabold text-center">Vásárlás összegzés</h1>
           <div className="bg-highlight h-1 mt-1 rounded-2xl w-[50%]"></div>
         </div>
-        <div className="flex flex-row gap-30">
+        <div className="flex flex-col sm:flex-row gap-10 sm:gap-30">
           <Card
             title="Cigányok származása"
             src="https://placehold.co/250x150"
